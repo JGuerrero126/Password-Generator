84d28165d5c47784f14160facb909b4cc00c37ef
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-01 18:22:12 -0800

    Added comments throughout the js to illustrate how the code works and what does what

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// These arrays are the seperate collections of character types used in the project
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "/", ".", ",", "<", ">", ";", ":", "'", "~","[", "]", "{", "}"]
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var numeric = [1,2,3,4,5,6,7,8,9,0]
+
+// This variable gets the button from the HTML and allows us to add to it much easier
 
 var generateBtn = document.querySelector("#generate");
 
@@ -13,15 +16,21 @@ function writePassword() {
 
 }
 
+// This tells the writePassword function to start once the "generate password" button is clicked on
 generateBtn.addEventListener("click", writePassword); { 
 };
 
+// This is the main function that does the entire process of making the password
+
 function generatePassword() {
+  // These variables are all necessary for this specific function so they all fit within this local scope
   var password = []
   var cPassword = []
   var choiceMade = false
 
   let numberOfChars = prompt("How many characters did you want to have in the password?")
+
+// This function checks to make sure the length requested is within the permitted parameters
 
   function validateCharLength (i) {
     if (i < 8 || i > 128) {
@@ -32,7 +41,11 @@ function generatePassword() {
     return numberOfChars
   }
 
+  // This calls the above function on the users input to make sure it fits within the parameters
+
   validateCharLength(numberOfChars);
+
+  // These variables are what asks the user what kind of characters they want and also put the specific array into a bigger array which the function later uses to make the password
 
   var isCapital = confirm("Do you want uppercase letters in it?")
     if (isCapital === true) {
@@ -58,10 +71,14 @@ function generatePassword() {
       choiceMade = true
     }
 
+    // This if statement checks to make sure a character type was selected and if not then it tells the user and restarts the process
+
   if (choiceMade === false) {
     alert("You didn't make any choices so the password is empty! click OK to start over!")
     return generatePassword();
   }
+
+// This is what actually takes the combined character types the user selected and makes them into the password
 
 for (var i = 0; i < parseInt(numberOfChars); i++) {
   var random = Math.floor(Math.random()*cPassword.length);
