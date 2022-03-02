35249ba8380a7a8987b4c2fc4bed1d03efa26d41
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-01 17:13:32 -0800

    I forgot to push as I was trying to make it

--- script.js
+++ script.js
@@ -1,53 +1,12 @@
-// user clicks button then prompt pops up asking for number of characters
-
-// hold all inputs from confirms in seperate variables
-
-// Do i need to hold any information?
-// create array with special characters
-// create array with numeric, 
-// create array lowercase
-// create array upper case
-
-// create empty array that holds all the true confirmed options in it.
-// push all character that user wants into this array. This creates the pool of characters to select from
-// once the pool is complete, then randomly select characters based on the number of characters the user selcted in the beggining.
-
-// generate password function
-// var numberOfChar = validatecharLength(prompts for number of characters) 
-// var isSpecialCharaters = confirm pops up asking if they would like specialial characters
-// var isCapitol = confirm for capital, 
-// var isLowercase = lowercase confirm, 
-// var isNumeric = numeric characters confirm
-
-// validate number of chars is between 8 and 128
-// validator function send it users input
-
-// function validateCharLength (numberOfChars) {
-//   if (numberOfChars < 8 || numberOfChars > 128) {
-//     alert("Please enter a number between 8 and 128");
-//     return generatePassword()
-//   }
-//   return numberOfChars
-// }
-
-// validate user selected at least one of the availbale options
-// if user selected isSpecial or isCapital or isLower or isNum then continue
-// else alert user "none selected" and return to main function call
-
-
-
-
-
-
-
-
 // Assignment Code
 
-
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "/", ".", ",", "<", ">", ";", ":", "'", "~","[", "]", "{", "}"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numeric = [1,2,3,4,5,6,7,8,9,0]
 
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -56,5 +15,60 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); { 
+};
+
+function generatePassword() {
+  var password = []
+  var cPassword = []
+  var choiceMade = false
+
+  let numberOfChars = prompt("How many characters did you want to have in the password?")
+
+  function validateCharLength (i) {
+    if (i < 8 || i > 128) {
+      i = 0
+      alert("Please enter a number between 8 and 128");
+      return generatePassword()
+    } 
+    return numberOfChars
+  }
+
+  validateCharLength(numberOfChars);
+
+  var isCapital = confirm("Do you want uppercase letters in it?")
+    if (isCapital === true) {
+    cPassword = cPassword.concat(upperCase)
+    choiceMade = true
+  }
+
+  var isLowercase = confirm("Did you want lower case letters in it?")
+    if (isLowercase === true) {
+      cPassword = cPassword.concat(lowerCase)
+      choiceMade = true
+    }
+
+  var isNumeric = confirm("How about numbers?")
+    if (isNumeric === true) {
+      cPassword = cPassword.concat(numeric)
+      choiceMade = true
+    }
+
+  var isSpecialCharaters = confirm("Did you want special characters to be included in it?")
+    if (isSpecialCharaters === true) {
+      cPassword = cPassword.concat(specialChar)
+      choiceMade = true
+    }
+
+  if (choiceMade === false) {
+    alert("You didn't make any choices so the password is empty! click OK to start over!")
+    return generatePassword();
+  }
+
+for (var i = 0; i < parseInt(numberOfChars); i++) {
+  var random = Math.floor(Math.random()*cPassword.length);
+  password +=cPassword[random]
+}
+
+return password
+}
